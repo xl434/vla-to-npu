fixed softmax testing

softmax/test_mapping_softmax.py
@@ -18,31 +18,28 @@ os.environ["ENABLE_AGGRESSIVE_PORT_UTILIZATION_PATCH"] = "1"
 # Model Configuration
 # ===============================================================================
 
-
-
-def test_mapping_softmax_8_512():
-    M = 2048
-    N = 512
-    P0_tile = 2
+def test_mapping_softmax_4_1024():
+    N = 1024
+    P0_tile = 4 ## this match to the kernel row size 
 
     KERNEL_LIB_PATH = "../cc/"
     Ty = bfloat16
 
-    SOFTMAX_P0 = N // P0_tile
+    SOFTMAX_P0 = N // P0_tile  # 256
     SOFTMAX_Ly = Layout("S0R")
 
     softmax = ExternalModule(
-        top="softmax_bf16_8_512",
+        top="softmax_bf16",
         impl_path=KERNEL_LIB_PATH + "softmax_bf16.cc",
         input_idx=[0],
         output_idx=[1],
     )
 
     def gen_softmax_primitives():
-        SOFTMAX_ROW = 16
+        SOFTMAX_ROW = 16    # number of physical tiles
         primitives = []
         for row in range(SOFTMAX_ROW):
-            if SOFTMAX_P0 // SOFTMAX_ROW > 1:
+            if SOFTMAX_P0 // SOFTMAX_ROW > 1:       # 16
                 primitives.append(
                     (
                         "bundle",
@@ -58,8 +55,8 @@ def test_mapping_softmax_8_512():
     def softmax_kernel():
         @df.kernel(mapping=[SOFTMAX_P0])
         def core(
-            input_x: Ty[M, N] @ SOFTMAX_Ly,
-            output_x: Ty[M, N] @ SOFTMAX_Ly,
+            input_x: Ty[N, N] @ SOFTMAX_Ly,
+            output_x: Ty[N, N] @ SOFTMAX_Ly,
         ):
             softmax(input_x, output_x)
 
@@ -70,14 +67,14 @@ def test_mapping_softmax_8_512():
         mapping_primitives=gen_softmax_primitives(),
         profile=True,
         warmup=20,
-        num_iters=100,
+        num_iters=1000,
     )
 
     # ---------------------------------------------------------
     # Generate inputs
     # ---------------------------------------------------------
-    input_tensor = np.random.randn(M, N).astype(np_bfloat16)
-    allo_out = np.zeros((M, N), dtype=np_bfloat16)
+    input_tensor = np.random.randn(N, N).astype(np_bfloat16)
+    allo_out = np.zeros((N, N), dtype=np_bfloat16)
 
     # Run Allo softmax
     softmax_mod(input_tensor, allo_out)
@@ -100,9 +97,9 @@ def test_mapping_softmax_8_512():
     )
     print("Softmax comparison PASSED!")
 
-def test_mapping_softmax_4_1024():
-    N = 1024
-    P0_tile = 4
+def test_mapping_softmax_8_512():
+    N = 512
+    P0_tile = 8
 
     KERNEL_LIB_PATH = "../cc/"
     Ty = bfloat16
@@ -111,7 +108,7 @@ def test_mapping_softmax_4_1024():
     SOFTMAX_Ly = Layout("S0R")
 
     softmax = ExternalModule(
-        top="softmax_bf16",
+        top="softmax_bf16_8_512",
         impl_path=KERNEL_LIB_PATH + "softmax_bf16.cc",
         input_idx=[0],
         output_idx=[1],
@@ -149,7 +146,7 @@ def test_mapping_softmax_4_1024():
         mapping_primitives=gen_softmax_primitives(),
         profile=True,
         warmup=20,
-        num_iters=100,
+        num_iters=1000,
     )
 
     # ---------------------------------------------------------
@@ -180,9 +177,8 @@ def test_mapping_softmax_4_1024():
     print("Softmax comparison PASSED!")
 
 def test_mapping_softmax_16_256():
-    M = 4096
     N = 256
-    P0_tile = 1
+    P0_tile = 16
 
     KERNEL_LIB_PATH = "../cc/"
     Ty = bfloat16
@@ -217,8 +213,8 @@ def test_mapping_softmax_16_256():
     def softmax_kernel():
         @df.kernel(mapping=[SOFTMAX_P0])
         def core(
-            input_x: Ty[M, N] @ SOFTMAX_Ly,
-            output_x: Ty[M, N] @ SOFTMAX_Ly,
+            input_x: Ty[N, N] @ SOFTMAX_Ly,
+            output_x: Ty[N, N] @ SOFTMAX_Ly,
         ):
             softmax(input_x, output_x)
 
@@ -229,14 +225,173 @@ def test_mapping_softmax_16_256():
         mapping_primitives=gen_softmax_primitives(),
         profile=True,
         warmup=20,
-        num_iters=100,
+        num_iters=1000,
     )
 
     # ---------------------------------------------------------
     # Generate inputs
     # ---------------------------------------------------------
-    input_tensor = np.random.randn(M, N).astype(np_bfloat16)
-    allo_out = np.zeros((M, N), dtype=np_bfloat16)
+    input_tensor = np.random.randn(N, N).astype(np_bfloat16)
+    allo_out = np.zeros((N, N), dtype=np_bfloat16)
+
+    # Run Allo softmax
+    softmax_mod(input_tensor, allo_out)
+
+    # ---------------------------------------------------------
+    # Torch reference
+    # ---------------------------------------------------------
+    torch_in = torch.from_numpy(input_tensor.astype(np.float32)).to(torch.bfloat16)
+    torch_out_fp32 = F.softmax(torch_in.float(), dim=-1)
+    torch_out_np = torch_out_fp32.cpu().numpy()
+
+
+    # ---------------------------------------------------------
+    # Compare
+    # ---------------------------------------------------------
+    np.testing.assert_allclose(
+        allo_out.astype(np.float32),
+        torch_out_np.astype(np.float32),
+        atol=1e-1, rtol=1e-2,
+    )
+    print("Softmax comparison PASSED!")
+
+def test_mapping_softmax_32_128():
+    N = 128
+    P0_tile = 32
+
+    KERNEL_LIB_PATH = "../cc/"
+    Ty = bfloat16
+
+    SOFTMAX_P0 = N // P0_tile
+    SOFTMAX_Ly = Layout("S0R")
+
+    softmax = ExternalModule(
+        top="softmax_bf16_32_128",
+        impl_path=KERNEL_LIB_PATH + "softmax_bf16.cc",
+        input_idx=[0],
+        output_idx=[1],
+    )
+
+    def gen_softmax_primitives():
+        SOFTMAX_ROW = 16
+        primitives = []
+        for row in range(SOFTMAX_ROW):
+            if SOFTMAX_P0 // SOFTMAX_ROW > 1:
+                primitives.append(
+                    (
+                        "bundle",
+                        [
+                            f"core_{SOFTMAX_ROW*i_+row}"
+                            for i_ in range(SOFTMAX_P0 // SOFTMAX_ROW)
+                        ],
+                    )
+                )
+        return primitives
+
+    @df.region()
+    def softmax_kernel():
+        @df.kernel(mapping=[SOFTMAX_P0])
+        def core(
+            input_x: Ty[N, N] @ SOFTMAX_Ly,
+            output_x: Ty[N, N] @ SOFTMAX_Ly,
+        ):
+            softmax(input_x, output_x)
+
+    softmax_mod = df.build(
+        softmax_kernel,
+        target="aie",
+        project="softmax.prj",
+        mapping_primitives=gen_softmax_primitives(),
+        profile=True,
+        warmup=20,
+        num_iters=1000,
+    )
+
+    # ---------------------------------------------------------
+    # Generate inputs
+    # ---------------------------------------------------------
+    input_tensor = np.random.randn(N, N).astype(np_bfloat16)
+    allo_out = np.zeros((N, N), dtype=np_bfloat16)
+
+    # Run Allo softmax
+    softmax_mod(input_tensor, allo_out)
+
+    # ---------------------------------------------------------
+    # Torch reference
+    # ---------------------------------------------------------
+    torch_in = torch.from_numpy(input_tensor.astype(np.float32)).to(torch.bfloat16)
+    torch_out_fp32 = F.softmax(torch_in.float(), dim=-1)
+    torch_out_np = torch_out_fp32.cpu().numpy()
+
+
+    # ---------------------------------------------------------
+    # Compare
+    # ---------------------------------------------------------
+    np.testing.assert_allclose(
+        allo_out.astype(np.float32),
+        torch_out_np.astype(np.float32),
+        atol=1e-1, rtol=1e-2,
+    )
+    print("Softmax comparison PASSED!")
+
+def test_mapping_softmax_64_64():
+    N = 64
+
+
+    KERNEL_LIB_PATH = "../cc/"
+    Ty = bfloat16
+
+    P0_tile = 64
+    SOFTMAX_P0 = N // P0_tile
+    SOFTMAX_Ly = Layout("S0R")
+
+    softmax = ExternalModule(
+        top="softmax_bf16_64_64",
+        impl_path=KERNEL_LIB_PATH + "softmax_bf16.cc",
+        input_idx=[0],
+        output_idx=[1],
+    )
+
+    def gen_softmax_primitives():
+        SOFTMAX_ROW = 1
+        primitives = []
+        for row in range(SOFTMAX_ROW):
+            if SOFTMAX_P0 // SOFTMAX_ROW > 1:
+                primitives.append(
+                    (
+                        "bundle",
+                        [
+                            f"core_{SOFTMAX_ROW*i_+row}"
+                            for i_ in range(SOFTMAX_P0 // SOFTMAX_ROW)
+                        ],
+                    )
+                )
+        return primitives
+
+    @df.region()
+    def softmax_kernel():
+        @df.kernel(mapping=[SOFTMAX_P0])
+        def core(
+            input_x: Ty[N, N] @ SOFTMAX_Ly,
+            output_x: Ty[N, N] @ SOFTMAX_Ly,
+        ):
+            softmax(input_x, output_x)
+
+    softmax_mod = df.build(
+        softmax_kernel,
+        target="aie",
+        project="softmax.prj",
+        mapping_primitives=gen_softmax_primitives(),
+        profile=True,
+        warmup=20,
+        num_iters=1000,
+    )
+
+    # ---------------------------------------------------------
+    # Generate inputs
+    # ---------------------------------------------------------
+    input_tensor = np.random.randn(N, N).astype(np_bfloat16)
+    allo_out = np.zeros((N, N), dtype=np_bfloat16)
 
     # Run Allo softmax
     softmax_mod(input_tensor, allo_out)
@@ -261,11 +416,8 @@ def test_mapping_softmax_16_256():
 
 
 if __name__ == "__main__":
-
-    # test_mapping_softmax_8_512()
     # test_mapping_softmax_4_1024()
-    test_mapping_softmax_16_256()
-
-
-
-
+    # test_mapping_softmax_8_512()
+    # test_mapping_softmax_16_256()
+    test_mapping_softmax_32_128()
+    # test_mapping_softmax_64_64()

softmax/test_tiled_softmax.py
@@ -21,7 +21,7 @@ Ly_1 = Layout("S1")
 # Softmax dimensions (same as masked test)
 SEQ_TILED = 64
 SEQ = 64
-HEAD_TILE = 3
+HEAD_TILE = 1
 
 # -----------------------------
 # PyTorch reference (UNMASKED)
@@ -73,7 +73,7 @@ def _test_softmax_float():
         target="aie",
         profile=True,
         warmup=20,
-        num_iters=100,  # execute multiple times for stable perf
+        num_iters=1000,  # execute multiple times for stable perf
     )
 
     output_allo = np.zeros((SEQ_TILED, SEQ * HEAD_TILE), dtype=np.float32)
@@ -135,7 +135,7 @@ def _test_softmax_bf16():
         target="aie",
         profile=True,
         warmup=20,
-        num_iters=100,  # execute multiple times for stable perf
+        num_iters=1000,  # execute multiple times for stable perf
     )
 
     output_allo = np.zeros((SEQ_TILED, SEQ * HEAD_TILE), dtype=np_bfloat16)