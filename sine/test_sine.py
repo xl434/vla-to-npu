4ea3356d812ae8d9bba6c5af2c260f9dc0d4a7f1
Tribeca <ck795@cornell.edu>
2026-03-27 03:08:28 -0400

    fixed sine

--- sine/test_sine.py
+++ sine/test_sine.py
@@ -8,14 +8,14 @@ import torch
 import ml_dtypes
 import allo.dataflow as df
 from allo.memory import Layout
-from allo.ir.types import bfloat16
+from allo.ir.types import float32
 from allo.backend.aie.external_kernel import ExternalModule
 
 # Matrix shape: rows=32, cols=64  (matches sin_float32 signature: [32][64])
 S = Layout.Shard
 R = Layout.Replicate
 Ly = [S(0), S(1)]
-Ty = bfloat16
+Ty = float32
 seq_tile = 32       # rows
 feature_tile = 64   # cols
 
@@ -41,7 +41,7 @@ def _mismatch_stats(actual: np.ndarray, expected: np.ndarray, rtol: float, atol:
 KERNEL_LIB_PATH = "../cc/float/"
 def _test_sine_single_tile():
     sine = ExternalModule(
-        top="sin_bfloat16",
+        top="sin_float32",
         impl_path=KERNEL_LIB_PATH + "sine.cc",
         input_idx=[0],
         output_idx=[1],
@@ -57,7 +57,7 @@ def _test_sine_single_tile():
             sine(local_input_x, local_output_x)
 
     torch.manual_seed(0)
-    input_tensor = (torch.rand(seq_tile, feature_tile, dtype=torch.bfloat16) * 40.0) - 20.0
+    input_tensor = (torch.rand(seq_tile, feature_tile, dtype=torch.float32) * 40.0) - 20.0
     ref_out = torch.sin(input_tensor)
     
     # CPU execution time
@@ -67,15 +67,22 @@ def _test_sine_single_tile():
     cpu_time_us = (end - start) * 1000000
 
     if "MLIR_AIE_INSTALL_DIR" in os.environ:
-        mod = df.build(top, target="aie")
-        output_allo = np.zeros((seq_tile, feature_tile), dtype=ml_dtypes.bfloat16)
-        input_numpy = input_tensor.view(torch.int16).cpu().numpy().view(ml_dtypes.bfloat16)
-        ref_numpy   = ref_out.view(torch.int16).cpu().numpy().view(ml_dtypes.bfloat16).astype(np.float32)
+        # mod = df.build(top, target="aie")
+        mod = df.build(
+            top,
+            target="aie",
+            profile=True,
+            trace=[("core", (0, 0))],
+            trace_size=65536,
+        )
+        output_allo = np.zeros((seq_tile, feature_tile), dtype=np.float32)
+        input_numpy = input_tensor.cpu().numpy()
+        ref_numpy   = ref_out.cpu().numpy()
 
         mod(input_numpy, output_allo)
 
         print(f"CPU execution time: {cpu_time_us:.2f} us")
-        output_allo = output_allo.astype(ml_dtypes.bfloat16)
+        output_allo = output_allo.astype(np.float32)
         try:
             np.testing.assert_allclose(output_allo, ref_numpy, rtol=RTOL, atol=ATOL)
             print(f"PASSED sine! (rtol={RTOL}, atol={ATOL})")
